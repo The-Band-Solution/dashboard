9cdb1e6249ff364a2e1fbb1feafb9938d6f5ccff
Paulo Sérgio dos Santos Júnior <paulossjunior@gmail.com>
2025-07-11 10:00:22 -0300

    adicionando o celery

--- src/dashboard/settings/base.py
+++ src/dashboard/settings/base.py
@@ -221,3 +221,7 @@ LOGGING = {
         },
     },
 }
+
+CELERY_BROKER_URL = os.getenv("CELERY_BROKER_URL", "redis://redis:6379/0")
+CELERY_ACCEPT_CONTENT = ["json"]
+CELERY_TASK_SERIALIZER = "json"
